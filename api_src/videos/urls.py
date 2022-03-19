7b1d9792968f6fe9459bb5b7d692b94a6fb973cc
roomey_rahman <roomeyrahman@gmail.com>
2022-03-20 02:06:14 +0600

    update

--- api_src/videos/urls.py
+++ api_src/videos/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ListCreateVideoAPIView.as_view(), name='get_post_videos'),
-    path('<int:pk>/', views.RetrieveUpdateDestroyVideoAPIView.as_view(), name='get_delete_update_video'),
+    path('<int:pk>', views.RetrieveUpdateDestroyVideoAPIView.as_view(), name='get_delete_update_video'),
 ]
